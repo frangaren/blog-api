409f0e20a73f051c757c9b7a7db5fef777148422
Francisco García <frangaren@usal.es>
2018-04-15 13:21:54 +0200

    Add code to handle not found endpoints

--- server.js
+++ server.js
@@ -14,4 +14,21 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(function (req, res, next) {
+    let error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use(function (err, req, res, next) {
+    const status = err.status || 500;
+    const message = err.message || 'Internal Server Error';
+    const reply = {
+        status: status,
+        message: message
+    };
+    res.status(status);
+    res.json(reply);
+});
+
 app.listen(port);

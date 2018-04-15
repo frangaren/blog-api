e6b78ee629d81b951fefbe3ebc42f25c17881fca
Francisco García <frangaren@usal.es>
2018-04-15 15:42:25 +0200

    Add user creation endpoint

--- router/api/v1/users.js
+++ router/api/v1/users.js
@@ -5,6 +5,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', listUsers);
+router.post('/', createUser);
 router.get('/:id', retrieveUser);
 
 function listUsers(req, res, next) {
@@ -20,6 +21,25 @@ function listUsers(req, res, next) {
         module: 'users',
         function: 'list',
         args: {}
+    });
+}
+
+function createUser(req, res, next) {
+    const worker = req.app.get('worker');
+    worker.once('message', function (msg) {
+        if (msg.success) {
+            res.json(msg.reply);
+        } else {
+            next(msg.error);
+        }
+    });
+    worker.send({
+        module: 'users',
+        function: 'create',
+        args: {
+            username: req.body.username,
+            password: req.body.password
+        }
     });
 }
 
--- worker/users.js
+++ worker/users.js
@@ -4,12 +4,20 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('User');
 
-exports.list = async function (params) {
+exports.list = async function (args) {
     const users = await User.find().exec();
     return users;
 }
 
-exports.retrieve = async function (params) {
-    const user = await User.find({_id: params.id}).exec();
+exports.retrieve = async function (args) {
+    const user = await User.find({_id: args.id}).exec();
+    return user;
+}
+
+exports.create = async function (args) {
+    let user = new User();
+    user.username = args.username;
+    user.password = args.password;
+    user = await user.save();
     return user;
 }

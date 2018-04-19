ff63baefc50216544a089d17c4be70a42a563011
Francisco García <frangaren@usal.es>
2018-04-19 12:37:54 +0200

    Add comment fields checking

--- router/api/v1/comments.js
+++ router/api/v1/comments.js
@@ -5,11 +5,22 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', listComments);
+
+router.post('/', requireAuthor);
+router.post('/', requirePost);
+router.post('/', requireText);
+router.post('/', existsAuthor);
+router.post('/', existsPost);
 router.post('/', createComment);
+
 router.get('/:id', existsComment);
 router.get('/:id', retrieveComment);
+
 router.put('/:id', existsComment);
+router.put('/:id', existsAuthor);
+router.put('/:id', existsPost);
 router.put('/:id', updateComment);
+
 router.delete('/:id', existsComment);
 router.delete('/:id', deleteComment);
 
@@ -109,7 +120,6 @@ function deleteComment(req, res, next) {
 function existsComment(req, res, next) {
     const worker = req.app.get('worker');
     worker.once('message', function (msg) {
-        console.log(msg);
         if (msg.success) {
             if (msg.reply) {
                 next();
@@ -122,7 +132,6 @@ function existsComment(req, res, next) {
             next(msg.error);
         }
     });
-    
     worker.send({
         module: 'comments',
         function: 'exists',
@@ -130,6 +139,90 @@ function existsComment(req, res, next) {
             id: req.params.id
         }
     });
+}
+
+function requireAuthor(req, res, next) {
+    if (!('author' in req.body)) {
+        let error = new Error('Author Not Provided');
+        error.status = 422;
+        next(error);
+    } else {
+        next();
+    }
+}
+
+function existsAuthor(req, res, next) {
+    if (!('author' in req.body)) {
+        next();
+    }
+    const worker = req.app.get('worker');
+    worker.once('message', function (msg) {
+        if (msg.success) {
+            if (msg.reply) {
+                next();
+            } else {
+                let error = new Error('Comment Author Not Found');
+                error.status = 404;
+                next(error);
+            }
+        } else {
+            next(msg.error);
+        }
+    });
+    worker.send({
+        module: 'users',
+        function: 'exists',
+        args: {
+            id: req.body.author
+        }
+    });
+}
+
+function requirePost(req, res, next) {
+    if (!('post' in req.body)) {
+        let error = new Error('Post Not Provided');
+        error.status = 422;
+        next(error);
+    } else {
+        next();
+    }
+}
+
+function existsPost(req, res, next) {
+    if (!('post' in req.body)) {
+        next();
+    }
+    const worker = req.app.get('worker');
+    worker.once('message', function (msg) {
+        if (msg.success) {
+            if (msg.reply) {
+                next();
+            } else {
+                let error = new Error('Post Not Found');
+                error.status = 404;
+                next(error);
+            }
+        } else {
+            next(msg.error);
+        }
+    });
+    worker.send({
+        module: 'posts',
+        function: 'exists',
+        args: {
+            id: req.body.post
+        }
+    });
+}
+
+function requireText(req, res, next) {
+    if (!('text' in req.body)) {
+        let error = new Error('Text Not Provided');
+        error.status = 422;
+        next(error);
+    } else {
+        next();
+    }
 }
 
 module.exports = router;

c04c1e6ad6f70fbc4e39d6dfd486e01b407a5b60
Francisco García <frangaren@usal.es>
2018-04-19 12:46:25 +0200

    Add post arguments checking

--- router/api/v1/posts.js
+++ router/api/v1/posts.js
@@ -5,11 +5,20 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', listPosts);
+
+router.post('/', requireAuthor);
+router.post('/', requireTitle);
+router.post('/', requireText);
+router.post('/', existsAuthor);
 router.post('/', createPost);
+
 router.get('/:id', existsPost);
 router.get('/:id', retrievePost);
+
 router.put('/:id', existsPost);
+router.put('/:id', existsAuthor);
 router.put('/:id', updatePost);
+
 router.delete('/:id', existsPost);
 router.delete('/:id', deletePost);
 
@@ -129,6 +138,63 @@ function existsPost(req, res, next) {
             id: req.params.id
         }
     });
+}
+
+function requireAuthor(req, res, next) {
+    if (!('author' in req.body)) {
+        let error = new Error('Author Not Provided');
+        error.status = 422;
+        next(error);
+    } else {
+        next();
+    }
+}
+
+function existsAuthor(req, res, next) {
+    if (!('author' in req.body)) {
+        next();
+    }
+    const worker = req.app.get('worker');
+    worker.once('message', function (msg) {
+        if (msg.success) {
+            if (msg.reply) {
+                next();
+            } else {
+                let error = new Error('Post Author Not Found');
+                error.status = 404;
+                next(error);
+            }
+        } else {
+            next(msg.error);
+        }
+    });
+    worker.send({
+        module: 'users',
+        function: 'exists',
+        args: {
+            id: req.body.author
+        }
+    });
+}
+
+function requireTitle(req, res, next) {
+    if (!('title' in req.body)) {
+        let error = new Error('Title Not Provided');
+        error.status = 422;
+        next(error);
+    } else {
+        next();
+    }
+}
+
+function requireText(req, res, next) {
+    if (!('text' in req.body)) {
+        let error = new Error('Text Not Provided');
+        error.status = 422;
+        next(error);
+    } else {
+        next();
+    }
 }
 
 module.exports = router;

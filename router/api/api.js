7b5ef01dad0702b4ef3b48a7d45b26a38165308f
Francisco García <frangaren@usal.es>
2018-04-21 21:01:29 +0200

    Add CORS headers

--- router/api/api.js
+++ router/api/api.js
@@ -6,6 +6,12 @@ const path = require('path');
 const router = express.Router();
 const v1Router = require(path.join(__dirname, 'v1', 'v1.js'));
 
+router.use('/', allowCORS);
 router.use('/v1/', v1Router);
 
+function allowCORS(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+}
 module.exports = router;

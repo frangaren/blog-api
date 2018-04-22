10172f6618b09abb54c40894eaed76fd8eccaff3
Francisco García <frangaren@usal.es>
2018-04-22 16:02:09 +0200

    Replaced busy wait with a more clever approach

--- worker-proxy.js
+++ worker-proxy.js
@@ -1,27 +1,60 @@
-const awaitsemaphore = require('await-semaphore');
+'use strict';
+
+class Worker {
+    constructor (worker) {
+        this.busy = false;
+        this.tasks = [];
+        this.worker = worker;
+    }
+
+    _runNext() {
+        const next = this.tasks.pop();
+        if (next) {
+            this._run(next);
+        } else {
+            this.busy = false;
+        }
+    }
+
+    _run(task) {
+        this.busy = true;
+        this.worker.send(task.message);
+        this.worker.once('message', (message) => {
+            if (message.success) {
+                task.resolve(message.reply);
+            } else {
+                task.resolve(new Error(message.error));
+            }
+            this._runNext();
+        });
+    }
+
+    schedule(message) {
+        const promise = new Promise((resolve, reject) => {
+            const task = {
+                message: message,
+                resolve: resolve,
+                reject: reject
+            };
+            if (this.busy == false) {
+                this._run(task);
+            } else {
+                this.tasks.push(task);
+            }
+        });
+        return promise;
+    }
+}
 
 function FunctionProxy(target, moduleProp) {
     const handler = {
         get: function (target, functionProp, receiver) {
             return function (args) {
-                const promise = new Promise((resolve, reject) => {
-                    target.mutex.acquire().then((release) => {
-                        target.worker.once('message', function (msg) {
-                            release();
-                            if (msg.success) {
-                                resolve(msg.reply);
-                            } else {
-                                reject(new Error(msg.error));
-                            }
-                        });
-                        target.worker.send({
-                            module: moduleProp,
-                            function: functionProp,
-                            args: args
-                        });
-                    });
+                return target.schedule({
+                    module: moduleProp,
+                    function: functionProp,
+                    args: args
                 });
-                return promise;
             }
         }
     }
@@ -29,16 +62,12 @@ function FunctionProxy(target, moduleProp) {
 }
 
 function ModuleProxy(worker) {
-    const target = {
-        worker: worker,
-        mutex: new awaitsemaphore.Mutex()
-    };
     const handler = {
         get: function (target, moduleProp, receiver) {
             return FunctionProxy(target, moduleProp);
         }
     }
-    return new Proxy(target, handler);
+    return new Proxy(new Worker(worker), handler);
 }
 
 module.exports = ModuleProxy;
